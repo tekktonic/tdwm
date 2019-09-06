093dbc11135f231eab1119d5c4840f0833c6d7d6
Daniel Wilkins <tekk@linuxmail.org>
2019-09-06 03:20:11 -0400

    Add mpd support to tdwmclock.

--- tdwmclock.sh
+++ tdwmclock.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 while [ true ]; do
-	xsetroot -name "| `tdwmtemp` | `date +'%a %b %e %H:%M'` | `tdwmbatt` |"
+	xsetroot -name "| `mpc status | head -n 1 | cut -d ' ' --complement -f 1 | head -c 100` | `tdwmtemp` | `date +'%a %b %e %H:%M'` | `tdwmbatt` |"
 	sleep 60
 done
