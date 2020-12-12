0b16297233b67595e5fddb92438428f78c7aca58
Daniel Wilkins <tekk@linuxmail.org>
2020-12-12 18:21:33 -0500

    Touch up tdwmclock

--- tdwmclock.sh
+++ tdwmclock.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 while [ true ]; do
-	xsetroot -name "| `mpc status | head -n 1 | cut -d ' ' --complement -f 1 | head -c 100` | `tdwmtemp` | `date +'%a %b %e %H:%M'` | `tdwmbatt` |"
+	xsetroot -name "| `mullvad status | awk -F ' - ' '{print $1}'` | `mpc status | head -n 1 | cut -d ' ' --complement -f 1 | head -c 100` | `tdwmtemp` | `date +'%a %b %e %H:%M'` | `tdwmbatt` |"
 	sleep 60
 done
